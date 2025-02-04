c58578f77d909cb72354c42c8a327ff7c8ca2043
Findus79 <mailto.andi@gmail.com>
2025-02-04 19:40:16 +0100

    Delete assets directory

--- assets/export_snowflakes_sprites.bat
+++
@@ -1,7 +0,0 @@
-echo "params: " %1 %2
-
-call "D:\Projekte\nintentool\bin\nintentool.exe" --inputdir %1 --project %2 --target snes --mode sprite --outdir D:/Projekte/Christmas/Data/Sprites ^
- --spritelayer name=snowflake_small_a tileoffset=0 bpp=4 ^
- --spritelayer name=snowflake_medium_a tileoffset=0 bpp=4
- 
- 
--- assets/export_titlescreen.bat
+++
@@ -1,5 +0,0 @@
-echo "params: " %1 %2
-
-call "D:\Projekte\nintentool\bin\nintentool.exe" --inputdir %1 --project %2 --target snes --mode map --outdir D:/Projekte/Christmas/Data/Titlescreen ^
- --layer name=Background width=32 height=28 bpp=4 tileorder=horizontal paloffset=0 header=0 ^
- --layer name=Foreground width=64 height=32 bpp=4 tileorder=horizontal paloffset=0 header=0 chunked=1
